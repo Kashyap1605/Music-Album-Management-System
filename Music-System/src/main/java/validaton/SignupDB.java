0c24acc5f7caf2f58fa8ebef8efb0b813fe815bd
abhishekY2401 <abhiyd2401@gmail.com>
2021-12-10 19:59:56 +0530

    added insertion command

--- Music-System/src/main/java/validaton/SignupDB.java
+++ Music-System/src/main/java/validaton/SignupDB.java
@@ -8,6 +8,26 @@ package validaton;
  *
  * @author KASHYAP
  */
+import java.sql.*;
+
 public class SignupDB {
-    
+    Connection connection;
+    Statement st;
+    ResultSet rs;
+
+    public void insert(String n, String s, String p) {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/music_db",
+                    "root",
+                    "Abhiraj@123");
+            st = connection.createStatement();
+            String query = "INSERT INTO user('"+n+"', '"+s+"', '"+p+"')";
+            rs = st.executeQuery(query);
+        
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
