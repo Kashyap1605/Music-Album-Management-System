9a0f8b01d0a8a9f256fd33c1c7c93efdeda80325
abhishekY2401 <abhiyd2401@gmail.com>
2021-12-10 20:03:57 +0530

    checking user details into database

--- Music-System/src/main/java/validaton/LoginDB.java
+++ Music-System/src/main/java/validaton/LoginDB.java
@@ -8,6 +8,34 @@ package validaton;
  *
  * @author KASHYAP
  */
+import java.sql.*;
+
 public class LoginDB {
-    
+    Connection connection;
+    Statement st;
+    ResultSet rs;
+    boolean login=false;
+
+    public boolean validate(String u, String p) {
+        try {
+            String query = "select * from user";
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/music_db",
+                    "root",
+                    "Abhiraj@123");
+            st = connection.createStatement();
+            rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                if (u.equals(rs.getString(2)) && p.equals(rs.getString(3))) {
+                    login = true;
+                    break;
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return login;
+    }
+
 }
