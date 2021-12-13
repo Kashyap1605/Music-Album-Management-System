62f8b8c7df63f1f379b0752648f76bfb63609cea
Kashyap Visavadia <knvisavadia1605@gmail.com>
2021-12-13 23:22:46 +0530

    secon Commit

--- Music-System/src/main/java/player/PlayerFrame.java
+++ Music-System/src/main/java/player/PlayerFrame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jobme.pk;
+package player;
 
 import jaco.mp3.player.MP3Player;
 import java.awt.Dimension;
